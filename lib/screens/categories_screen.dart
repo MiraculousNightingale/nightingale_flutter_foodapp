d6521cb7aaa6f0526453c44ccc89b7e0c04bc3ae
Nightingale <esteticwence@gmail.com>
2023-07-13 18:39:13 +0300

    Add Categories Grid animation

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -3,25 +3,63 @@ import 'package:nightingale_flutter_foodapp/widgets/category_grid_item.dart';
 
 import '../dummy_data.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 325),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    Future.delayed(
+      const Duration(milliseconds: 175),
+      () => _animationController.forward(),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(25),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(_animationController),
+        child: child,
       ),
-      children: [
-        for (final category in DUMMY_CATEGORIES)
-          CategoryGridItem(
-            category: category,
-          )
-      ],
+      child: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in DUMMY_CATEGORIES)
+            CategoryGridItem(
+              category: category,
+            )
+        ],
+      ),
     );
   }
 }
